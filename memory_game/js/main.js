057a359a45bb7879b85b1e4b50b88c9b3dba126e
unknown <timothyfrederick.ku@gmail.com>
2019-06-17 01:28:11 +0800

    Changed array to objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,45 @@
 console.log("Up and running");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: 'queen',
+		suits: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	}, 
+	{
+		rank: 'queen',
+		suits: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	}, 
+	{		
+		rank: 'king',
+		suits: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	}, 
+	{
+		rank: 'king',
+		suits: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}];
+
 var cardsInPlay = [];
 
 function checkForMatch () { 
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
+			console.log("You found a match!");
 		} else {
-			alert("Sorry, try again.");
+			console.log("Sorry, try again.");
 		}
 	}
 }
 
 function flipCard (cardId) {
-	console.log ("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log ("User flipped " + cards[cardId].rank + ' ' + cards[cardId].suits);
+	console.log (cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
-
 
 
 flipCard(0);
