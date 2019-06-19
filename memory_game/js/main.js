28bafbea553145cbc3033d818f011c75172e05ee
unknown <timothyfrederick.ku@gmail.com>
2019-06-19 23:37:46 +0800

    Added DOM

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -27,20 +27,30 @@ var cardsInPlay = [];
 function checkForMatch () { 
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]){
-			console.log("You found a match!");
+			alert("You found a match!");
 		} else {
-			console.log("Sorry, try again.");
+			alert("Sorry, try again.");
 		}
 	}
 }
 
-function flipCard (cardId) {
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
 	console.log ("User flipped " + cards[cardId].rank + ' ' + cards[cardId].suits);
-	console.log (cards[cardId].cardImage)
+	this.setAttribute ('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
 
-flipCard(0);
-flipCard(2);
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener ('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
